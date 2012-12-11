3b096a1e57b04db4352358bce671dd71f37459dd
jhubert <jhubert@gmail.com>
2012-12-10 21:41:07 -0800

    Add the latest locale files

---
+++ .localeapp/config.rb
@@ -0,0 +1,6 @@
+Localeapp.configure do |config|
+  config.api_key                    = 'qqmsF6K49YAIgajFTSRh4tGGkzw54rsQOPe7rvarsiHWlhu7TA'
+  config.translation_data_directory = 'locales'
+  config.synchronization_data_file  = '.localeapp/log.yml'
+  config.daemon_pid_file            = '.localeapp/localeapp.pid'
+end
